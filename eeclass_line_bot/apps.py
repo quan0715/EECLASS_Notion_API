ca0d5e116169b1439df97abfd5e61a4b6a1dff17
quan0715 <quan787887@gmail.com>
2023-06-14 00:39:06 +0800

    first commit

---
+++ eeclass_line_bot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EeclassLineBotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'eeclass_line_bot'
---
+++ eeclass_line_bot/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                m_text = event.message.text
+                message = [TextSendMessage(text=m_text)]
+                line_bot_api.reply_message(event.reply_token, message)
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()

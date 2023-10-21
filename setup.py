39247f8a310f3360afa877c390f5e80bc9b2cc64
張百寬 <quan@zhangbaikuandeMacBook-Pro.local>
2023-10-21 11:55:41 +0800

    upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='eeclass_bot',
-    version='0.0.41',
+    version='0.0.5',
     author='Quan',
     author_email='quan787887@gmail.com',
     packages=find_packages(
